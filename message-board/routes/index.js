b5b4db18fb6a1f9ae24a33efeb7603879baa66e6
Mustafa <mustafamcm9@gmail.com>
2023-03-29 17:10:18 +0300

    messages obj

--- message-board/routes/index.js
+++ message-board/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  { text: "Hi how are you ?", user: "Dane", added: new Date() },
+  { text: "Hello World!", user: "Sarah", added: new Date() },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages });
 });
 
-module.exports = router;
+module.exports = {
+  router,
+  messages,
+};
